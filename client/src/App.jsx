6a2f381e7f462e4ab604c96396bf505425f3b1f6
your <jaiswalanshikaajay.7@gmail.com>
2024-03-12 23:26:26 +0530

    singnp fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { CampaignDetails, CreateCampaign, UpdateCampaign , Home, Profile , LandingPage, SignUp, Registration } from './pages';
+import { CampaignDetails, CreateCampaign, UpdateCampaign, Home, Profile, LandingPage, Registration } from './pages';
 
 
 const App = () => {
   return (
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          {/* <Route path="/login" element={<Login />} /> */}
-          <Route path="/registrationpage" element={<Registration />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/home" element={<Home />} /> 
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-campaign" element={<CreateCampaign />} />
-          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
-          <Route path="/campaign-update/:id" element={<UpdateCampaign />} />
-        </Routes>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      {/* <Route path="/login" element={<Login />} /> */}
+      <Route path="/registrationpage" element={<Registration />} />
+
+      <Route path="/home" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/create-campaign" element={<CreateCampaign />} />
+      <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+      <Route path="/campaign-update/:id" element={<UpdateCampaign />} />
+    </Routes>
   )
 }
 
